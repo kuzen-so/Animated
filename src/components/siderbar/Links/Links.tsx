5ede7c21c2999ad22877e45c019eb7e4a1092f5f
kuzen <xiaokundaa@gmail.com>
2024-07-29 23:07:26 +0800

    chore: 添加side打开动画

--- src/components/siderbar/Links/Links.tsx
+++ src/components/siderbar/Links/Links.tsx
@@ -1,3 +1,29 @@
+import {motion} from 'framer-motion'
+
+const variants = {
+  open:{
+    transition: {
+      staggerChildren:0.2,
+  }},
+  closed:{
+    transition:{
+      staggerChildren:0.05,
+      staggerDirection:1
+    }
+  }
+}
+
+const itemVariants = {
+  open:{
+    y:0,
+    opacity:1,
+  },
+    
+  closed:{
+    y:50,
+    opacity:0,
+  }
+}
 
 
 const Links = () => {
@@ -5,7 +31,7 @@ const Links = () => {
 
     const items = ["主页",  "博客","项目","留言", "关于"];
   return (
-    <div className="links">{items.map((item)=>(<a href={`#${item}`} key={item}>{item}</a>))}</div>
+    <motion.div className="links" variants={variants}>{items.map((item)=>(<motion.a href={`#${item}`} key={item} variants={itemVariants}>{item}</motion.a>))}</motion.div>
   )
 }
 
