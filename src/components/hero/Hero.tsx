4f4fa3378399787196c3c9feddcae7576e934126
kuzen <xiaokundaa@gmail.com>
2024-07-30 00:49:39 +0800

    chore: 优化主页动画

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,25 +1,55 @@
 import "./hero.scss"
+import {animate, motion, stagger} from "framer-motion"
+
+const textVariants = {
+    initial:{
+        x:-500,
+        opacity:0,
+    },
+    animate:{
+        x:0,
+        opacity:1,
+        transition:{
+            duration:1,
+            staggerChildren:0.1,
+        }
+    },
+    scrollButton:{
+        opacity:0,
+        y:10,
+        transition:{
+            duration:2,
+            repeat:Infinity,
+        }
+    }
+}
+
 
 const Hero = () => {
   return (
     <div className="hero">
         <div className="wrapper">
             {/* 主页文字 */}
-            <div className="textContainer">
-                <h2>KUZEN SCHOLAR</h2>
-                <h1>Web developer </h1>
-                <h1>UI desiginer</h1>
+            <motion.div className="textContainer"
+            variants={textVariants}
+            initial="initial"
+            animate="animate"
+            >
+                <motion.h2  variants={textVariants}>KUZEN SCHOLAR</motion.h2>
+                <motion.h1  variants={textVariants}>Web developer </motion.h1>
+                <motion.h1  variants={textVariants}>UI desiginer</motion.h1>
                 {/* 主页按钮 */}
-                <div className="buttons">
-                    <button>最新作品</button>
-                    <button>联系我</button>
-                </div>
-                <img src="/Scroll.png" alt="" />
-             </div>
-             <div className="slidingTextContainer">
+                <motion.div variants={textVariants} className="buttons">
+                    <motion.button>最新作品</motion.button>
+                    <motion.button>联系我</motion.button>
+                </motion.div>
+                <motion.img   variants={textVariants} animate="scrollButton" src="/Scroll.png" alt="" />
+             </motion.div>
+             {/* 背景文字 */}
+             <motion.div  variants={textVariants} className="slidingTextContainer">
                 Writer Content Creator Influencer
-             </div>
-        {/* 主页照片 */}
+             </motion.div>
+             {/* 主页照片 */}
             <div className="imageContainer">
             <img src="/hero.png" alt="" />
             </div>
